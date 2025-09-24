91beb8f96cfe43b9c9ba0ff483a5390a8bcf09c4
Vinicnio Brejinski <viniciosdb@gmail.com>
2025-09-24 08:51:11 -0300

    create pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import './styles/global.css'
 import './styles/theme.css'
 
 import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+import { AboutPomodoro } from './pages/AboutPomodoro'
 
 export function App() {
-  return <Home />
+  return <AboutPomodoro />
 }
